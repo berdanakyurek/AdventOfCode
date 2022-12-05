455992ce3fc8139d42fea81ae2af322d69e3cbb6
Berdan <berdan.akyurek@ug.bilkent.edu.tr>
2022-12-05 15:50:02 +0300

    day 5

---
+++ 2022/Day5/day5-1.py
@@ -0,0 +1,36 @@
+lines = open("input.txt").readlines()
+
+
+stacks = []
+
+for i in range(len(lines[0])//4):
+    stacks.append([])
+
+move = False
+
+for line in lines:
+    if line.strip() == "":
+        move = True
+        continue
+
+    if not move:
+        for i in range(len(line)):
+            letter = line[i];
+            if letter.isalpha():
+                stacks[(i-1)//4].insert(0, letter)
+    
+    else:
+        words = line.split()
+        count = int(words[1])
+        frm = int(words[3]) - 1
+        to = int(words[5]) - 1
+
+        if frm == to:
+            continue
+        
+        for i in range(count):
+            stacks[to].append(stacks[frm].pop())
+            
+for i in stacks:
+    print(i[-1], end="")
+print()
